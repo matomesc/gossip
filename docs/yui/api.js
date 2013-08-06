959e9a9390521975cd66ac226337c4e67925eeab
Mihai Tomescu <matomesc@gmail.com>
2013-08-06 02:11:38 -0400

    added docs

---
+++ docs/yui/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cluster",
+        "Hub",
+        "Message",
+        "Node",
+        "utils"
+    ],
+    "modules": [
+        "core"
+    ],
+    "allModules": [
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "Cluster data structure.\n\nResponsible for maintaing the state of the nodes in the cluster."
+        }
+    ]
+} };
+});
